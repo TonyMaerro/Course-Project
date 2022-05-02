020a86cc708bb52c73b6c5653dbe86229eb466f9
Dima Gaykov <dima.gaykov.04@gmail.com>
2022-05-02 22:42:56 +0300

    5 раздел

---
+++ JS/xml.js
@@ -0,0 +1,23 @@
+var xmlDoc;
+let xmlFile = "../XML/products.xml";
+let xmlhttp;
+let prods_for = Array.from(document.querySelectorAll(".product"));
+if (window.XMLHttpRequest) { // для IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+} else { // для IE6, IE5
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET", xmlFile, false);
+xmlhttp.setRequestHeader("Content-Type", "text/xml");
+xmlhttp.send(null);
+xmlDoc = xmlhttp.responseXML;
+console.log(xmlDoc);
+let prods_from = xmlDoc.querySelectorAll("product");
+for (let i = 0; i < prods_for.length; i++) {
+    prods_for[i].querySelector(".product_title").innerText = prods_from[i].querySelector("title").textContent;
+    prods_for[i].querySelector(".description").innerText = prods_from[i].querySelector("desc").textContent;
+    prods_for[i].querySelector(".cost").innerText = prods_from[i].querySelector("cost").textContent;
+    prods_for[i].querySelector("img").src = prods_from[i].querySelector("ref-img").textContent;
+
+}
