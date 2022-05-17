9a8b052e86fc7a81c47a4c941cad1d2f4f6e4627
Dima Gaykov <dima.gaykov.04@gmail.com>
2022-05-17 12:52:46 +0300

    full documents

--- JS/xml.js
+++ JS/xml.js
@@ -12,12 +12,10 @@ xmlhttp.open("GET", xmlFile, false);
 xmlhttp.setRequestHeader("Content-Type", "text/xml");
 xmlhttp.send(null);
 xmlDoc = xmlhttp.responseXML;
-console.log(xmlDoc);
 let prods_from = xmlDoc.querySelectorAll("product");
 for (let i = 0; i < prods_for.length; i++) {
     prods_for[i].querySelector(".product_title").innerText = prods_from[i].querySelector("title").textContent;
     prods_for[i].querySelector(".description").innerText = prods_from[i].querySelector("desc").textContent;
     prods_for[i].querySelector(".cost").innerText = prods_from[i].querySelector("cost").textContent;
     prods_for[i].querySelector("img").src = prods_from[i].querySelector("ref-img").textContent;
-
 }
